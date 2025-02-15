5df1357f4cffbca0e346a4a326ae78eb2bae875f
Silent Cat <ngocphuocnguyen312@gmail.com>
2025-02-15 09:39:25 +0700

    add files

---
+++ lib/remote_hooks.dart
@@ -0,0 +1,10 @@
+/// remote_hooks, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate remote_hooks
+/// dart pub global activate remote_hooks
+///
+/// # see usage
+/// remote_hooks --help
+/// ```
+library;
